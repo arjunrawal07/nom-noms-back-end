3599887e3d65ebbda2dd80f4a99497327e3357ef
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-04-20 23:35:40 -0400

    data is stored as data.json

--- db/getRecipes.js
+++ db/getRecipes.js
@@ -1,36 +1,22 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
 
-const apiURL = "https://api.edamam.com/search?q=chicken";
+const apiURL = "https://api.edamam.com/search?q=recipe";
 const apiKey = "&app_key=a589f24078d846c9bdba02ecd2fab13f";
 const apiId = "&app_id=72de889b";
-
-// const getRecipes = async (...ingredients) => {
-//   const searchedIngredients = ingredients
-//     .map((ingredient, i) => {
-//       if (i < ingredients.length - 1) {
-//         return ingredient + "+";
-//       } else {
-//         return ingredient;
-//       }
-//     })
-//     .join("");
 
 const url = `${apiURL}${apiId}${apiKey}`;
 
 fetch(url)
   .then((res) => res.json())
   .then((res) => {
-    console.log(res);
-  });
-//   let recipes = JSON.stringify(res);
-//   fs.writeFile("./db/data.json", recipes, (err) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("data is converted");
-//     }
-//   });
-// };
+    let recipes = JSON.stringify(res);
 
-// getRecipes("chicken");
+    fs.writeFile("./db/data.json", recipes, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("data is converted");
+      }
+    });
+  });
